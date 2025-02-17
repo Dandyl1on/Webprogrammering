2a3e82baf764dc5c22bf9082fe060fea4e1a2d52
Goodest person <mflens22@student.aau.dk>
2025-02-17 14:14:18 +0100

    Div work

--- Index.js
+++ Index.js
@@ -1,4 +1,12 @@
-$('#Confirm').click(function(){
-    var inputtext = $('#TextInput').val();
-    $('#one').text(inputtext);
+$('#Confirm1').click(function(){
+    var inputtext1 = $('#TextInput1').val();
+    $('li#one').text(inputtext1);
+})
+$('#Confirm2').click(function () {
+    var inputtext2 = $('#TextInput2').val()
+    $('li#two').text(inputtext2);
+})
+$('#Confirm3').click(function () {
+    var inputtext3 = $('#TextInput3').val()
+    $('li#three').text(inputtext3);
 })
